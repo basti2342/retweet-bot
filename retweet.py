11a5241518d6932be5b3ec93dbaea2c46e6b6fa8
basti2342 <basti@randomprojects.de>
2015-02-15 16:37:50 +0100

    changed syntax to python3

--- retweet.py
+++ retweet.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, ConfigParser, tweepy, inspect, hashlib
+import os, configparser, tweepy, inspect, hashlib
 
 path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 # read config
-config = ConfigParser.SafeConfigParser()
+config = configparser.SafeConfigParser()
 config.read(os.path.join(path, "config"))
 
 # your hashtag or search query and tweet language (empty = all languages)
@@ -18,7 +18,7 @@ userBlacklist = []
 wordBlacklist = ["RT", u"♺"]
 
 # build savepoint path + file
-hashedHashtag = hashlib.md5(hashtag).hexdigest()
+hashedHashtag = hashlib.md5(hashtag.encode("utf-8")).hexdigest()
 last_id_filename = "last_id_hashtag_%s" % hashedHashtag
 rt_bot_path = os.path.dirname(os.path.abspath(__file__))
 last_id_file = os.path.join(rt_bot_path, last_id_filename)
@@ -34,7 +34,7 @@ try:
 		savepoint = file.read()
 except IOError:
 	savepoint = ""
-	print "No savepoint found. Trying to get as many results as possible."
+	print("No savepoint found. Trying to get as many results as possible.")
 
 # search query
 timelineIterator = tweepy.Cursor(api.search, q=hashtag, since_id=savepoint, lang=tweetLanguage).items()
@@ -53,7 +53,7 @@ except IndexError:
 timeline = filter(lambda status: status.text[0] != "@", timeline)
 timeline = filter(lambda status: not any(word in status.text.split() for word in wordBlacklist), timeline)
 timeline = filter(lambda status: status.author.screen_name not in userBlacklist, timeline)
-timeline.reverse()
+timeline = reversed(list(timeline))
 
 tw_counter = 0
 err_counter = 0
@@ -61,20 +61,20 @@ err_counter = 0
 # iterate the timeline and retweet
 for status in timeline:
 	try:
-		print "(%(date)s) %(name)s: %(message)s\n" % \
+		print("(%(date)s) %(name)s: %(message)s\n" % \
 			{ "date" : status.created_at,
 			"name" : status.author.screen_name.encode('utf-8'),
-			"message" : status.text.encode('utf-8') }
+			"message" : status.text.encode('utf-8') })
 
 		api.retweet(status.id)
 		tw_counter += 1
 	except tweepy.error.TweepError as e:
 		# just in case tweet got deleted in the meantime or already retweeted
 		err_counter += 1
-		#print e
+		print(e)
 		continue
 
-print "Finished. %d Tweets retweeted, %d errors occured." % (tw_counter, err_counter)
+print("Finished. %d Tweets retweeted, %d errors occured." % (tw_counter, err_counter))
 
 # write last retweeted tweet id to file
 with open(last_id_file, "w") as file:
