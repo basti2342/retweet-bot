02bb590bbd5c2ce17e254e843ed4e517354f107c
MChambers1992 <michael.chambers@bedegaming.com>
2018-01-06 20:01:46 +0000

    [MajorVersion] In progress refactoring to follow PEP8 and other recommended linter rules

--- retweet.py
+++ retweet.py
@@ -1,86 +1,102 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, configparser, tweepy, inspect, hashlib
+import logging
+import configparser
+import hashlib
+import inspect
+import os
 
-path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+import tweepy
 
-# read config
-config = configparser.SafeConfigParser()
-config.read(os.path.join(path, "config"))
+if __name__ == '__main__':
+    logging.debug("Bot started")
 
-# your hashtag or search query and tweet language (empty = all languages)
-hashtag = config.get("settings", "search_query")
-tweetLanguage = config.get("settings", "tweet_language")
+    PATH = os.path.dirname(os.path.abspath(
+        inspect.getfile(inspect.currentframe())))
 
-# Number retweets per time
-num = int(config.get("settings","number_of_rt"))
+    # read config
+    CONFIG = configparser.SafeConfigParser()
+    CONFIG.read(os.path.join(PATH, "config"))
 
-# blacklisted users and words
-userBlacklist = []
-wordBlacklist = ["RT", u"♺"]
+    # your hashtag or search query and tweet language (empty = all languages)
+    HASHTAG = CONFIG.get("settings", "search_query")
+    TWEET_LANG = CONFIG.get("settings", "tweet_language")
 
-# build savepoint path + file
-hashedHashtag = hashlib.md5(hashtag.encode('ascii')).hexdigest()
-last_id_filename = "last_id_hashtag_%s" % hashedHashtag
-rt_bot_path = os.path.dirname(os.path.abspath(__file__))
-last_id_file = os.path.join(rt_bot_path, last_id_filename)
+    # Number retweets per time
+    RATE_LIMIT = int(CONFIG.get("settings", "number_of_rt"))
 
-# create bot
-auth = tweepy.OAuthHandler(config.get("twitter", "consumer_key"), config.get("twitter", "consumer_secret"))
-auth.set_access_token(config.get("twitter", "access_token"), config.get("twitter", "access_token_secret"))
-api = tweepy.API(auth)
+    # blacklisted users and words
+    USER_BLACKLIST = []
+    WORD_BLACKLIST = ["RT", u"♺"]
 
-# retrieve last savepoint if available
-try:
-    with open(last_id_file, "r") as file:
-        savepoint = file.read()
-except IOError:
-    savepoint = ""
-    print("No savepoint found. Bot is now searching for results")
+    # build savepoint path + file
+    HASHED_HASHTAG = hashlib.md5(HASHTAG.encode('ascii')).hexdigest()
+    LAST_FILENAME_ID = "last_id_hashtag_%s" % HASHED_HASHTAG
+    BOT_PATH = os.path.dirname(os.path.abspath(__file__))
+    LAST_FILE_ID = os.path.join(BOT_PATH, LAST_FILENAME_ID)
 
-# search query
-timelineIterator = tweepy.Cursor(api.search, q=hashtag, since_id=savepoint, lang=tweetLanguage).items(num)
+    # create bot
+    AUTH = tweepy.OAuthHandler(CONFIG.get(
+        "twitter", "consumer_key"), CONFIG.get("twitter", "consumer_secret"))
+    AUTH.set_access_token(CONFIG.get("twitter", "access_token"),
+                          CONFIG.get("twitter", "access_token_secret"))
+    API = tweepy.API(AUTH)
 
-# put everything into a list to be able to sort/filter
-timeline = []
-for status in timelineIterator:
-    timeline.append(status)
-
-
-try:
-    last_tweet_id = timeline[0].id
-except IndexError:
-    last_tweet_id = savepoint
-
-# filter @replies/blacklisted words & users out and reverse timeline
-#timeline = filter(lambda status: status.text[0] = "@", timeline)   - uncomment to remove all tweets with an @mention
-timeline = filter(lambda status: not any(word in status.text.split() for word in wordBlacklist), timeline)
-timeline = filter(lambda status: status.author.screen_name not in userBlacklist, timeline)
-timeline = list(timeline)
-timeline.reverse()
-
-tw_counter = 0
-err_counter = 0
-
-# iterate the timeline and retweet
-for status in timeline:
+    # retrieve last savepoint if available
     try:
-        print("(%(date)s) %(name)s: %(message)s\n" % \
-              {"date": status.created_at,
-               "name": status.author.screen_name.encode('utf-8'),
-               "message": status.text.encode('utf-8')})
+        with open(LAST_FILE_ID, "r") as file:
+            SAVEPOINT = file.read()
+    except IOError:
+        SAVEPOINT = ""
+        print ("No savepoint found. Bot is now searching for results")
 
-        api.retweet(status.id)
-        tw_counter += 1
-    except tweepy.error.TweepError as e:
-        # just in case tweet got deleted in the meantime or already retweeted
-        err_counter += 1
-        # print e
-        continue
+    # search query
+    TIMELINE_ITERATOR = tweepy.Cursor(
+        API.search, q=HASHTAG, since_id=SAVEPOINT, lang=TWEET_LANG).items(RATE_LIMIT)
 
-print("Finished. %d Tweets retweeted, %d errors occured." % (tw_counter, err_counter))
+    # put everything into a list to be able to sort/filter
+    TIMELINE = []
+    for status in TIMELINE_ITERATOR:
+        TIMELINE.append(status)
 
-# write last retweeted tweet id to file
-with open(last_id_file, "w") as file:
-    file.write(str(last_tweet_id))
+    try:
+        LAST_TWEET_ID = TIMELINE[0].id
+    except IndexError:
+        LAST_TWEET_ID = SAVEPOINT
+
+    # filter @replies/blacklisted words & users out and reverse timeline
+
+    # uncomment to remove all tweets with an @mention
+    # timeline = filter(lambda status: status.text[0] = "@", timeline)
+    TIMELINE = filter(lambda status: not any(word in status.text.split()
+                                             for word in WORD_BLACKLIST), TIMELINE)
+    TIMELINE = filter(
+        lambda status: status.author.screen_name not in USER_BLACKLIST, TIMELINE)
+    TIMELINE = list(TIMELINE)
+    TIMELINE.reverse()
+
+    TW_COUNT = 0
+    ERR_COUNT = 0
+
+    # Iterate the timeline and retweet
+    # In case tweet gets deleted in the meantime or already retweeted throw and print error
+    for status in TIMELINE:
+        try:
+            print("(%(date)s) %(name)s: %(message)s\n" %
+                  {"date": status.created_at,
+                   "name": status.author.screen_name.encode('utf-8'),
+                   "message": status.text.encode('utf-8')})
+
+            API.retweet(status.id)
+            TW_COUNT += 1
+        except tweepy.error.TweepError as err:
+            ERR_COUNT += 1
+            logging.error(err)
+            continue
+
+    print ("Finished. %d Tweets retweeted, %d errors occured." % (TW_COUNT, ERR_COUNT))
+
+    # write last retweeted tweet id to file
+    with open(LAST_FILE_ID, "w") as file:
+        file.write(str(LAST_TWEET_ID))
