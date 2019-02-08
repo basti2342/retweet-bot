2f6b96e8d59937f6cfd439606e6731b68fa296b9
Michael Chambers <michael.chambers1992@gmail.com>
2019-02-08 15:09:42 +0000

    [MinorVersion] Add option to aggressively remove non-followers (#12)

--- src/follower_management.py
+++ src/follower_management.py
@@ -10,31 +10,85 @@ def manage_followers(api, config):
     """Performs the logic surrounding maintaining active follower using the follower management settings defined in the config file"""
 
     if config.follower_management['manage_followers'] and common_methods.can_perform_action_today(config.follower_management['days_to_manage']) is True:
-        unfollowed_count = 0
-        err_count = 0
+        
+        if config.follower_management['aggressive_management'] is True:
+            aggressive_management(api, config)
+        else:
+            passive_management(api, config)
 
-        inactivityDate = datetime.date.today() - datetime.timedelta(days = config.follower_management['inactivity_period'])
 
-        followers = api.friends_ids(config.twitter_keys['screen_name'])
-        for follower in followers:
-            try:
-                if (unfollowed_count < config.follower_management['max_unfollows']):
-                    lastTweet = api.user_timeline(follower, count = 1)
-                    lastTweetDate = lastTweet[0].created_at.date()
+def aggressive_management(api, config):
+    """Aggressive management unfollows any followers until rate limit is hit that aren't following back"""
 
-                    if lastTweetDate < inactivityDate:
-                        api.destroy_friendship(follower)
-                        unfollowed_count += 1
+    limit_hit = False
+    unfollowed_count = 0
+    err_count = 0
+
+    try:
+        for page in tweepy.Cursor(api.friends, count = 100).pages():  
+            if limit_hit is False:      
+                user_ids = [user.id for user in page]
+
+                for user in api._lookup_friendships(user_ids):
+                    try:
+                        if (unfollowed_count > config.follower_management['max_unfollows']):
+                            limit_hit = True
+                            break
+
+                        if not user.is_followed_by:
+                            api.destroy_friendship(user.id)
+                            unfollowed_count += 1
+
+                    except (tweepy.error.TweepError, IndexError) as err:
+                        err_count += 1
+                        logging.error(err)   
+
+                        if (hasattr(err, 'reason')):
+                            if err.reason == "[{'message': 'Rate limit exceeded', 'code': 88}]":
+                                limit_hit = True
+                                break 
+
                         continue
 
-            except (tweepy.error.TweepError, IndexError) as err:
-                err_count += 1
-                logging.error(err)   
+    except (tweepy.error.TweepError, IndexError) as err:
+        err_count += 1
+        logging.error(err)   
 
-                if (hasattr(err, 'reason')):
-                    if err.reason == "[{'message': 'Rate limit exceeded', 'code': 88}]":
-                        break 
+        if (hasattr(err, 'reason')):
+            if err.reason == "[{'message': 'Rate limit exceeded', 'code': 88}]":
+                limit_hit = True
 
-                continue
+    print("Finished. %d unfollowed, %d errors." % (unfollowed_count, err_count))
 
-        print("Finished. %d unfollowed, %d errors." % (unfollowed_count, err_count))    
+
+def passive_management(api, config):
+    """Passive management unfollows any followers who haven't tweeted within the inactivity date set in config"""
+
+    unfollowed_count = 0
+    err_count = 0
+
+    inactivityDate = datetime.date.today() - datetime.timedelta(days = config.follower_management['inactivity_period'])
+
+    followers = api.friends_ids(config.twitter_keys['screen_name'])
+    for follower in followers:
+        try:
+            if (unfollowed_count < config.follower_management['max_unfollows']):
+                lastTweet = api.user_timeline(follower, count = 1)
+                lastTweetDate = lastTweet[0].created_at.date()
+
+                if lastTweetDate < inactivityDate:
+                    api.destroy_friendship(follower)
+                    unfollowed_count += 1
+                    continue
+
+        except (tweepy.error.TweepError, IndexError) as err:
+            err_count += 1
+            logging.error(err)   
+
+            if (hasattr(err, 'reason')):
+                if err.reason == "[{'message': 'Rate limit exceeded', 'code': 88}]":
+                    break 
+
+            continue
+
+    print("Finished. %d unfollowed, %d errors." % (unfollowed_count, err_count))
